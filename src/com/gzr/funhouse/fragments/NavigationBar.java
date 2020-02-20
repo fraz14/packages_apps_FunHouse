e6c34b9d2187545d0d0b6474b2cd041ecbd314a0
Alex Cruz <du.alexcruz@gmail.com>
2020-02-19 23:31:11 -0500

    Navbar: Add setting to toggle navbar
    - Enable navbar options
    
    Change-Id: Id2efd9bc0b8246ab7bce545553677d539d9ad55c

--- src/com/gzr/funhouse/fragments/NavigationBar.java
+++ src/com/gzr/funhouse/fragments/NavigationBar.java
@@ -15,20 +15,67 @@
  */
 package com.gzr.funhouse.fragments;
 
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
 import com.android.internal.logging.nano.MetricsProto;
 
-import android.os.Bundle;
 import com.android.settings.R;
-
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
-public class NavigationBar extends SettingsPreferenceFragment {
+import com.gzr.funhouse.preference.SystemSettingSwitchPreference;
+
+public class NavigationBar extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_NAVIGATION_BAR_ENABLED = "force_show_navbar";
+
+    private SwitchPreference mNavigationBar;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.navigationbar);
+
+        final PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        final boolean defaultToNavigationBar = getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar);
+        final boolean navigationBarEnabled = Settings.System.getIntForUser(
+                resolver, Settings.System.FORCE_SHOW_NAVBAR,
+                defaultToNavigationBar ? 1 : 0, UserHandle.USER_CURRENT) != 0;
+
+        mNavigationBar = (SwitchPreference) findPreference(KEY_NAVIGATION_BAR_ENABLED);
+        mNavigationBar.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR,
+                defaultToNavigationBar ? 1 : 0) == 1));
+        mNavigationBar.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mNavigationBar) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FORCE_SHOW_NAVBAR, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
